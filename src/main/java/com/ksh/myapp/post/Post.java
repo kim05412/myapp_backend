3dab92eb0033b87097e4e06d4a2863dd4d207155
Sohyun kim <rlathgus541@naver.com>
2023-08-14 09:49:39 +0900

    posting

--- src/main/java/com/ksh/myapp/post/Post.java
+++ src/main/java/com/ksh/myapp/post/Post.java
@@ -1,4 +1,20 @@
 package com.ksh.myapp.post;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Post {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private String typeOfRestaurant;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    private String creatorName;
+    private long creatorId;
+
+    private Long createdTime;
+
 }
