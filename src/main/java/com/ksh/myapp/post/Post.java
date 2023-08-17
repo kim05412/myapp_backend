38c95407b094d6890a557bd5b61ed4b5d476f79f
Sohyun kim <rlathgus541@naver.com>
2023-08-17 19:53:51 +0900

    post 진행중

---
+++ src/main/java/com/ksh/myapp/post/Post.java
@@ -0,0 +1,22 @@
+package com.ksh.myapp.post;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor //빈 생성자
+@AllArgsConstructor //초기화
+@Builder  // 타입에 맞춰 자동 할당
+
+@Data
+@Entity  //JPA->DB 테이블 연결
+public class Post {
+    @Id // DB 테이블의 PK로 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    DB의 자동 증가(auto-increment)-> 기본 키(primary key) 값을 생성
+    private long id; //null(x)
+    private
+
+}
---
+++ src/main/java/com/ksh/myapp/post/PostController.java
@@ -0,0 +1,9 @@
+package com.ksh.myapp.post;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+}
